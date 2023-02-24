5e40a96892fb1744cbe2d696c431bb60aa89b924
ranger <southorange1228@foxmail.com>
2023-02-24 20:36:18 +0800

    feat(sciprt): support clean root folder

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -2,9 +2,10 @@ import * as rimraf from "rimraf";
 import * as glob from "glob";
 
 const clean = async () => {
-  const paths = glob.sync(
-    "./packages/**/{.cargo,.turbo,dist,target,node_modules}"
-  );
+  const paths = [
+    ...glob.sync("./packages/**/{.cargo,.turbo,dist,target,node_modules}"),
+    ...glob.sync("./{.cargo,.turbo,dist,target,node_modules}"),
+  ];
   console.log("🫣 Start to clean some folders or files...");
   paths.forEach((i) => {
     rimraf.rimrafSync(i);
